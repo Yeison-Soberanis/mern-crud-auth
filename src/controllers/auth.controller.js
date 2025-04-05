e782b354a6726662e855b1c7f86400b59576d2db
Yeison-Soberanis <yeisonvi13@gmail.com>
2025-04-05 00:02:24 -0600

    Implement user registration with JWT and email check

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from '../models/user.model.js'
 import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
+import { createAccessToken } from '../libs/jwt.js'
 
 export const register = async (req, res) => {
     const {email, password, username} = req.body;
@@ -18,36 +18,19 @@ export const register = async (req, res) => {
 
         // Se guarda el nuevo usuario en la base de datos
         const userSaved = await newUser.save();
-
-        // Se crea un token para el nuevo usuario
-        jwt.sign(
-            {
-                id: userSaved._id,
-            },
-            "secret123",
-            {
-                expiresIn: "1d",
-            },
-            (err, token) => {
-                if (err) console.log(err);
-                res.cookie('token', token)
-                res.json({
-                    mensaje: 'user created successfully',
-                })
-            }
-        );
-
-        // res.json({
-        //     id: userSaved._id,
-        //     username: userSaved.username,
-        //     email: userSaved.email,
-        //     createdAt: userSaved.createdAt,
-        //     updatedAt: userSaved.updatedAt,
-        // });
+        const token = await createAccessToken({ id: userSaved._id }) //Se crea un token para el nuevo usuario
+        
+        res.cookie('token', token);
+        res.json({
+            id: userSaved._id,
+            username: userSaved.username,
+            email: userSaved.email,
+            createdAt: userSaved.createdAt,
+            updatedAt: userSaved.updatedAt,
+        });
     } catch (error) {
-        console.log(error);
-    }
-    
-}
+      res.status(500).json({ mensaje: error.message });
+    }   
+};
 
 export const login = (req, res) => res.send('Login');
