baa2c3e37eb66bc26f1652a5a3fea21178196e40
Yeison-Soberanis <yeisonvi13@gmail.com>
2025-05-17 17:06:33 -0600

    the user registration section has been completed

---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,44 @@
+import { useForm } from 'react-hook-form';
+import { registerRequest } from '../api/auth'; 
+
+function RegisterPage() {
+  const { register, handleSubmit } = useForm(); 
+
+  const onSubmit = handleSubmit( async (values) => {
+    const res = await registerRequest(values);
+    console.log(res);
+  })
+
+  return (
+    <div className="bg-zinc-800 max-w-md p-10 rounded-md">
+      <form onSubmit = {onSubmit}>
+        <input
+          type="text"
+          {...register("username", { required: true })}
+          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+          placeholder="Username"
+        />
+
+        <input
+          type="email"
+          {...register("email", { required: true })}
+          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+          placeholder="Email"
+        />
+
+        <input
+          type="password"
+          {...register("password", { required: true })}
+          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+          placeholder="Password"
+        />
+
+        <button type="submit" className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-2 w-full">
+        Register
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default RegisterPage;
--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.routes.js";
 import tasksRoutes from "./routes/tasks.routes.js";
@@ -8,6 +9,11 @@ import tasksRoutes from "./routes/tasks.routes.js";
 // Creación de un servidor express basico
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+); // Para que todos los dominios puedan comunicar con el servidor
 app.use(morgan("dev")); // Middleware para registrar las peticiones HTTP en la consola
 app.use(express.json()); // Middleware para parsear el cuerpo de las peticiones en formato JSON
 app.use(cookieParser()); // Middleware para parsear las cookies de las peticiones
